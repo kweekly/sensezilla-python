9036d0bac04ad23545eb2237c3fb0cace12062a5
Sensezilla User for running power monitoring code <sensezilla@sensezilla.eecs.berkeley.edu>
2012-11-13 00:12:39 -0800

    QRdata reads from devicedb

--- web/qrdata.py
+++ web/qrdata.py
@@ -9,31 +9,97 @@ from jinja2 import *
 import httplib
 
 CODES = {
-    "d00001" : "818a5336-0f4d-5088-a1a9-14e4e4410fa2",
-    "d00002" : "ff250610-ad0f-5055-a726-35733494b6e5",
-    "d00003" : "fbc2a542-3719-5046-aebe-f0c68e41e231",
-    "d00004" : "a586d399-cdfb-5a8a-a4cd-f3642823b4ee",
-    #"d00005" : "d5a8842f-e5fb-5e10-bfe1-a340c0cb7a24",
-    #"d00006" : "b79ce629-9393-55d7-8862-762c2bcc2cb3"
+    "d00001" : ("P","0013A2004090FEA4", 0),
+    "d00002" : ("P","0013A2004090FEA4", 1),
+    "d00003" : ("P","0013A2004090FEA4", 2),
+    "d00004" : ("P","0013A2004090FEA4", 3),
+    "d00005" : ("C","00:12:6d:45:50:7f:77:4b_disaggregated", 3),
+    "d00006" : ("C","00:12:6d:45:50:7f:77:4b_disaggregated", 4),
  };
+
+CODE_LINES = {}
+con = None;
+ 
+def get_smap_data(uuid):
+    global con
+    con.request("GET","/backend/api/prev/uuid/%s?starttime=%d&format=csv&tags=&"%(uuid,int(1000*time.time())));
+    bmsresp = con.getresponse()
+    datlines = bmsresp.read().splitlines()
+    tm = 0;
+    val = -1;
+    for dl in datlines:
+        if len(dl) > 0 and dl[0] != '#' and ',' in dl:
+            tm,val = dl.split(',')
+            break
+            
+    return float(val)
+ 
+lookedstuffup = False
+def lookupstuff():
+    global lookedstuffup, CODE_LINES
+    if lookedstuffup:
+        return 
+        
+    import config
+    import utils
+    import devicedb
+    import postgresops
+    devicedb.connect()
     
+    for code,dat in CODES.iteritems():
+        if dat[0] == "P":
+            dev = devicedb.get_devices(where="idstr='%s'"%dat[1],limit=1);
+            if len(dev) != 1:
+                return lambda:"ERROR: Can't find device";
+            dev = dev[0]
+            # get plug load names
+            metas = devicedb.get_devicemetas(where="key like 'PLUGLOAD%d' and %d=any(devices)"%(dat[2],dev.ID),limit=1);
+            if len(metas) != 1 or metas[0].parent == 0:
+                loadstr = "UNKNOWN";
+            else:
+                meta = metas[0]
+                if meta.parent == 1:
+                    loads = [int(s) for s in meta.value.split(',')]
+                else:
+                    loads = [meta.parent]
+                
+                loadstr = ""
+                for lid in loads:
+                    loadnamemeta = devicedb.get_devicemetas(where="id=%d"%lid,limit=1)[0]
+                    loadstr += loadnamemeta.value;
+                    if lid != loads[-1]:
+                        loadstr += " + "
+                        
+            # get uuid of power and current
+            devdef = utils.read_device(dev.device_type)
+            poweruuid = None
+            currentuuid = None
+            for feednameidx in range(len(devdef['feeds'])):
+                feedname = devdef['feeds'][feednameidx]
+                if 'power %d'%(dat[2]+1) in feedname.lower():
+                    poweruuid = dev.source_ids[feednameidx]
+                elif 'current %d'%(dat[2]+1) in feedname.lower():
+                    currentuuid = dev.source_ids[feednameidx]
+        
+            CODE_LINES[code] = lambda loadstr=loadstr,poweruuid=poweruuid,currentuuid=currentuuid:  ("Device(s): "+loadstr + "\n" +
+                                        "Wattage: %.2f W"%(get_smap_data(poweruuid)) + "\n" + 
+                                        "Amperage: %.2f A"%(get_smap_data(currentuuid)));
+                
+        else:
+            CODE_LINES[code] = lambda:"NOT IMPLEMENTED"
+    
+    lookedstuffup = True
+
 
 def do_qrdata(environ, start_response):
+    global con
+    lookupstuff()
+
     start_response('200 OK',[('Content-Type','text/plain')])
     con = httplib.HTTPConnection("sensezilla.berkeley.edu");
     resp = []
-    for code,uid in CODES.iteritems():
-        con.request("GET","/backend/api/prev/uuid/%s?starttime=%d&format=csv&tags=&"%(uid,int(1000*time.time())));
-        bmsresp = con.getresponse()
-        datlines = bmsresp.read().splitlines()
-        tm = 0;
-        val = -1;
-        for dl in datlines:
-            
-            if len(dl) > 0 and dl[0] != '#' and ',' in dl:
-                tm,val = dl.split(',')
-                break
-                
-        resp.append("%s:%.5f\n"%(code,float(val)));
+    for code in CODES.keys():
+        resp.append("%s;%s\n"%(code,CODE_LINES[code]().replace('\n',';')));
+        
     con.close();
     return resp;
